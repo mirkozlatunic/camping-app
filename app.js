999ee6717d59988361337c1f89746730678c1ee4
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-12-24 20:01:57 -0800

    first working setup of the app.js file

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+const methodOverride = require('method-override');
 
 const app = express();
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/test');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');
