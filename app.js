ee800813e02b118f693119c0afa203c02c5f3544
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-01-16 21:38:10 -0800

    setup of testuser with register

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -40,10 +43,22 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
+});
+
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'm.zlatttt@gmail', username: 'mzlat' });
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
 });
 
 app.use('/campgrounds', campgrounds);
