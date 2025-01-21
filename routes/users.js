85f7d463e24a9ea9797009dce0821103ea457a5d
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-01-20 16:44:11 -0800

    redirect to last page when loggin in

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
@@ -15,8 +16,11 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash('success', 'Welcome to Yelp Camp!');
-      res.redirect('/campgrounds');
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+      });
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('register');
@@ -30,13 +34,15 @@ router.get('/login', (req, res) => {
 
 router.post(
   '/login',
+  storeReturnTo,
   passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login',
   }),
   (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl);
   }
 );
 
