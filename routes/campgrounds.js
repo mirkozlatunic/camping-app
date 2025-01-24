b83c83754540387c3b163ac6e38a90c9beff5d42
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-01-23 16:28:56 -0800

    setup of .env

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,8 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router
   .route('/')
