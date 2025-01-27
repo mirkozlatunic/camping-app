0b683caef57b88851142141f79cb3a3eb9b6349c
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-01-26 22:19:52 -0800

    setup of maptiler

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,7 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+const maptilerClient = require('@maptiler/client');
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,7 +13,12 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.features[0].geometry;
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
