0a4707a315482eb1aedf1cae5c5c12b690e32fb8
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-01-28 21:47:30 -0800

    add map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -9,9 +9,9 @@ const map = new maptilersdk.Map({
 
 new maptilersdk.Marker()
   .setLngLat(campground.geometry.coordinates)
-  //   .setPopup(
-  //     new maptilersdk.Popup({ offset: 25 }).setHTML(
-  //       `<h3>${campground.title}</h3><p>${campground.location}</p>`
-  //     )
-  //   )
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
   .addTo(map);
