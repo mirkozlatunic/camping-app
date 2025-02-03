c307698809f519c4b0ffa24c29dac7933e4cfe5c
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-02-02 20:52:34 -0800

    setup security with helmet

--- app.js
+++ app.js
@@ -40,6 +40,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanizite());
 app.use(helmet());
 
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  // "https://api.tiles.mapbox.com/",
+  // "https://api.mapbox.com/",
+  'https://kit.fontawesome.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net',
+  'https://cdn.maptiler.com/', // add this
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com/',
+  'https://stackpath.bootstrapcdn.com/',
+  // "https://api.mapbox.com/",
+  // "https://api.tiles.mapbox.com/",
+  'https://fonts.googleapis.com/',
+  'https://use.fontawesome.com/',
+  'https://cdn.jsdelivr.net',
+  'https://cdn.maptiler.com/', // add this
+];
+const connectSrcUrls = [
+  // "https://api.mapbox.com/",
+  // "https://a.tiles.mapbox.com/",
+  // "https://b.tiles.mapbox.com/",
+  // "https://events.mapbox.com/",
+  'https://api.maptiler.com/', // add this
+];
+
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        'https://res.cloudinary.com/douqbebwk/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        'https://images.unsplash.com/',
+        'https://api.maptiler.com/',
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
+
 const sessionConfig = {
   name: 'session',
   secret: 'thisshouldabettersecret',
